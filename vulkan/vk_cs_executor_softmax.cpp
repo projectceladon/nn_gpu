5f03f9783eaefeffdbf4bd158e81f66dc1a3014b
yifangmx <yifangx.ma@intel.com>
2019-05-28 11:44:52 +0800

    modification on vulkan-based Softmax

--- vulkan/vk_cs_executor_softmax.cpp
+++ vulkan/vk_cs_executor_softmax.cpp
@@ -25,10 +25,13 @@
 
 NAME_SPACE_BEGIN
 
-#define LOCAL_SZ_X 256
+#define LOCAL_SZ_X 8
 
 struct SoftmaxShaderConfig
 {
+public:
+    SoftmaxShaderConfig(int lsx) : local_size_x(lsx) {};
+
     int local_size_x;
 };
 
@@ -101,68 +104,83 @@ bool VkCsExecutor::doSOFTMAX(const Operation& operation)
 
 #else
 
-/* this implemenation mainly follow vkcom's solution, but still failed to run,
- * confused on that, need studied into the shader code
- */
 struct SoftmaxParam {
-    int channel_size;
-    int outer_size;
-    int channels;
-    int logsoftmax;
+    int array_size;
+    float beta;
+    int total;
 };
 
 /* todo: shader is not ready, vkcom softmax shader seems not match with android tensorflow */
 bool VkCsExecutor::doSOFTMAX(const Operation& operation)
 {
     NN_GPU_ENTRY();
+
 #define BUFFER_NUM 2
     opBase->initVulkanThing(BUFFER_NUM);
 
     ASSERT(operation.type == OperationType::SOFTMAX);
-    const hidl_vec<uint32_t>& ins = operation.inputs;
-    const hidl_vec<uint32_t>& outs = operation.outputs;
-    SoftmaxParam param;
-    SoftmaxShaderConfig config;
 
-    if (outs.size() != 1 || ins.size() != 2) {
+    const hidl_vec<uint32_t>& ins  = operation.inputs;
+    const hidl_vec<uint32_t>& outs = operation.outputs;
+
+    /* local_size_y and local_size_z is not set */
+    SoftmaxShaderConfig config(LOCAL_SZ_X);
+
+    if (outs.size() != 1 || ins.size() != 2)
+    {
+        LOGE("invalid inputs/outputs size");
         return false;
     }
 
-    VkOperand& in = operands[ins[0]];
+    VkOperand& input  = operands[ins[0]];
     VkOperand& output = operands[outs[0]];
 
-    /* todo: hard code for rank=2 */
-    param.channels = in.getDimensionSize(1);
-    param.channel_size = 1;
-    param.outer_size = in.getElementCount(0, 1);
-    param.logsoftmax = 0;
-    NN_GPU_DEBUG("param channels is %d, channel_size is %d, outer_size is %d, logsoftmax is %d",
-        param.channels, param.channel_size, param.outer_size, param.logsoftmax);
+    int dim_num = input.getNumberOfDimensions();
+    ASSERT(dim_num == 2 || dim_num == 4);
 
-    uint32_t total = in.getDimensionSize(0);   // one work item per batch
+    uint32_t arraySize = input.getDimensionSize(1);
 
-    /* local_size_y and local_size_z is not set */
-    config.local_size_x = 8;
+    if (dim_num == 4)
+    {
+        arraySize *= (input.getDimensionSize(2) * input.getDimensionSize(3));
+    }
+
+    uint32_t total = input.getDimensionSize(0);   // one work item per batch
+
+    SoftmaxParam param;
+    param.array_size = arraySize;
+    param.beta       = operands[ins[1]].getScalarData<float>();
+    param.total      = total;
+
+    NN_GPU_DEBUG("param array_size is %d, beta is %f, total is %d", param.array_size, param.beta, param.total);
 
     if (opBase->pipeline == VK_NULL_HANDLE)
     {
+        NN_GPU_DEBUG("do createShaderModule");
         opBase->createShaderModule(softmax_spv, sizeof(softmax_spv));
+
+        NN_GPU_DEBUG("do createPipeline");
         opBase->createPipeline(sizeof(SoftmaxParam));
     }
 
-    opBase->bindOperand(in, 0, opBase->descriptor_set);
+    NN_GPU_DEBUG("bind operands");
+    opBase->bindOperand(input, 0, opBase->descriptor_set);
     opBase->bindOperand(output, 1, opBase->descriptor_set);
-    opBase->group_x = alignSize(total, config.local_size_x) / config.local_size_x;
+
+    opBase->group_x = (total + config.local_size_x - 1) / config.local_size_x;
     opBase->group_y = 1;
     opBase->group_z = 1;
 
+    NN_GPU_DEBUG("do recordCommandBuffer");
     opBase->recordCommandBuffer((void *)&param, sizeof(SoftmaxParam));
+
+    NN_GPU_DEBUG("do runCommandBuffer");
     opBase->runCommandBuffer();
 
     output.dump();
     NN_GPU_EXIT();
  
-	return true;
+    return true;
 }
 
 #endif
