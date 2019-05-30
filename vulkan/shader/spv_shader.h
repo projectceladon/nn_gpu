f25798c7078bf748f98061baad08d65cbe86ca77
yifangmx <yifangx.ma@intel.com>
2019-05-30 13:45:31 +0800

    modification on vulkan-based convolution operation
        1. Fix the crash issue: when vulkan-based convolution runs for multiple times, it causes a crash.
        2. Remove unused parameters from ConvParameter.

--- vulkan/shader/spv_shader.h
+++ vulkan/shader/spv_shader.h
@@ -22,7 +22,7 @@
 NAME_SPACE_BEGIN
 
 extern const unsigned int elewise_spv[890];
-extern const unsigned int conv_spv[1847];
+extern const unsigned int conv_spv[1739];
 extern const unsigned int concat_spv[541];
 extern const unsigned int softmax_spv[900];
 extern const unsigned int avg_pool_spv[1538];
