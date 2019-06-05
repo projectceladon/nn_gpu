02d634722209bd6eede99aac30e428c35eb0f749
yifangma <yifangx.ma@intel.com>
2019-06-05 14:33:45 +0800

    modify vulkan-based convolution     Set all the paramters as specialization constants, allowing shader to have local workgroup size changed at runtime.

--- vulkan/vk_cs_executor_conv.cpp
+++ vulkan/vk_cs_executor_conv.cpp
@@ -30,14 +30,23 @@ NAME_SPACE_BEGIN
 #define MAX_GROUP_COUNT_Y 65535
 #define MAX_GROUP_COUNT_Z 65535
 
-struct ConvParameter {
+struct PushConst {
 public:
-    ConvParameter(int ih, int iw, int oh, int ow, int fh, int fw, int chn,
-                  int bat, int M, int K, int N):
-        in_h(ih), in_w(iw), out_h(oh), out_w(ow), filter_h(fh), filter_w(fw), channels(chn),
-        batch(bat), m(M), k(K), n(N)
+    PushConst() {};
+};
+
+struct SpecializaitonConst {
+public:
+    SpecializaitonConst() {};
+    SpecializaitonConst(int ih, int iw, int oh, int ow, int fh,
+                        int fw, int chn, int bat, int M, int K, int N):
+        in_h(ih), in_w(iw), out_h(oh), out_w(ow), filter_h(fh), filter_w(fw),
+        channels(chn), batch(bat), m(M), k(K), n(N)
     {};
 
+    int local_sz_x;
+    int local_sz_y;
+    int local_sz_z;
     int in_h;
     int in_w;
     int out_h;
@@ -113,10 +122,24 @@ bool VkCsExecutor::convolve(const Operation& operation, ShaderConfig& config)
 
     PaddingScheme padding_mode;
 
-    ConvParameter param(in_shape[kShapeIdxHeight], in_shape[kShapeIdxWidth],
-                        out_shape[kShapeIdxHeight], out_shape[kShapeIdxWidth],
-                        filter_shape[kShapeIdxHeight], filter_shape[kShapeIdxWidth],
-                        in_shape[kShapeIdxChannel], in_shape[kShapeIdxBatch], M, K, N);
+    SpecializaitonConst spec_const(in_shape[kShapeIdxHeight], in_shape[kShapeIdxWidth],
+                                   out_shape[kShapeIdxHeight], out_shape[kShapeIdxWidth],
+                                   filter_shape[kShapeIdxHeight], filter_shape[kShapeIdxWidth],
+                                   in_shape[kShapeIdxChannel], in_shape[kShapeIdxBatch], M, K, N);
+
+    // todo: need a wrapper, just for 3*3*1 && 3*3*2 input here.
+    spec_const.local_sz_x = 1;
+    spec_const.local_sz_y = 16;
+    if (in_shape[kShapeIdxChannel] == 1)
+    {
+        spec_const.local_sz_z = 1;
+    }
+    else
+    {
+        spec_const.local_sz_z = 4;
+    }
+
+    PushConst push_const;
 
     if (opBase->pipeline == VK_NULL_HANDLE)
     {
@@ -127,11 +150,11 @@ bool VkCsExecutor::convolve(const Operation& operation, ShaderConfig& config)
             uint32_t padding_top    = operands[ins[5]].getScalarData<uint32_t>();
             uint32_t padding_bottom = operands[ins[6]].getScalarData<uint32_t>();
 
-            param.pad_w             = padding_left;
-            param.pad_h             = padding_top;
-            param.stride_w          = operands[ins[7]].getScalarData<uint32_t>();
-            param.stride_h          = operands[ins[8]].getScalarData<uint32_t>();
-            param.activation        = operands[ins[9]].getScalarData<uint32_t>();
+            spec_const.pad_w        = padding_left;
+            spec_const.pad_h        = padding_top;
+            spec_const.stride_w     = operands[ins[7]].getScalarData<uint32_t>();
+            spec_const.stride_h     = operands[ins[8]].getScalarData<uint32_t>();
+            spec_const.activation   = operands[ins[9]].getScalarData<uint32_t>();
 
             // assert(spec_const.activation == 0);  todo: add activation
 
@@ -146,27 +169,60 @@ bool VkCsExecutor::convolve(const Operation& operation, ShaderConfig& config)
         }
         else
         {
-            padding_mode     = static_cast<PaddingScheme>(operands[ins[3]].getScalarData<uint32_t>());
-            param.stride_w   = operands[ins[4]].getScalarData<uint32_t>();
-            param.stride_h   = operands[ins[5]].getScalarData<uint32_t>();
-            param.activation = operands[ins[6]].getScalarData<uint32_t>();
+            padding_mode          = static_cast<PaddingScheme>(operands[ins[3]].getScalarData<uint32_t>());
+            spec_const.stride_w   = operands[ins[4]].getScalarData<uint32_t>();
+            spec_const.stride_h   = operands[ins[5]].getScalarData<uint32_t>();
+            spec_const.activation = operands[ins[6]].getScalarData<uint32_t>();
 
-            assert(param.activation == 0);
+            assert(spec_const.activation == 0);
 
-            calculateExplicitPadding(param.in_w, param.stride_w, param.filter_w, padding_mode, &param.pad_w);
-            calculateExplicitPadding(param.in_h, param.stride_h, param.filter_h, padding_mode, &param.pad_h);
+            calculateExplicitPadding(spec_const.in_w, spec_const.stride_w, spec_const.filter_w,
+                                     padding_mode, &spec_const.pad_w);
+            calculateExplicitPadding(spec_const.in_h, spec_const.stride_h, spec_const.filter_h,
+                                     padding_mode, &spec_const.pad_h);
         }
+
+#define SPEC_CONST_NUM 19
+        VkSpecializationMapEntry entry[SPEC_CONST_NUM];
+
+        SET_SPEC_CONST_ENTRY(entry[0], 0, offsetof(SpecializaitonConst, local_sz_x), sizeof(int));
+        SET_SPEC_CONST_ENTRY(entry[1], 1, offsetof(SpecializaitonConst, local_sz_y), sizeof(int));
+        SET_SPEC_CONST_ENTRY(entry[2], 2, offsetof(SpecializaitonConst, local_sz_z), sizeof(int));
+        SET_SPEC_CONST_ENTRY(entry[3], 3, offsetof(SpecializaitonConst, in_h), sizeof(int));
+        SET_SPEC_CONST_ENTRY(entry[4], 4, offsetof(SpecializaitonConst, in_w), sizeof(int));
+        SET_SPEC_CONST_ENTRY(entry[5], 5, offsetof(SpecializaitonConst, out_h), sizeof(int));
+        SET_SPEC_CONST_ENTRY(entry[6], 6, offsetof(SpecializaitonConst, out_w), sizeof(int));
+        SET_SPEC_CONST_ENTRY(entry[7], 7, offsetof(SpecializaitonConst, stride_h), sizeof(int));
+        SET_SPEC_CONST_ENTRY(entry[8], 8, offsetof(SpecializaitonConst, stride_w), sizeof(int));
+        SET_SPEC_CONST_ENTRY(entry[9], 9, offsetof(SpecializaitonConst, pad_h), sizeof(int));
+        SET_SPEC_CONST_ENTRY(entry[10], 10, offsetof(SpecializaitonConst, pad_w), sizeof(int));
+        SET_SPEC_CONST_ENTRY(entry[11], 11, offsetof(SpecializaitonConst, filter_h), sizeof(int));
+        SET_SPEC_CONST_ENTRY(entry[12], 12, offsetof(SpecializaitonConst, filter_w), sizeof(int));
+        SET_SPEC_CONST_ENTRY(entry[13], 13, offsetof(SpecializaitonConst, channels), sizeof(int));
+        SET_SPEC_CONST_ENTRY(entry[14], 14, offsetof(SpecializaitonConst, batch), sizeof(int));
+        SET_SPEC_CONST_ENTRY(entry[15], 15, offsetof(SpecializaitonConst, m), sizeof(int));
+        SET_SPEC_CONST_ENTRY(entry[16], 16, offsetof(SpecializaitonConst, k), sizeof(int));
+        SET_SPEC_CONST_ENTRY(entry[17], 17, offsetof(SpecializaitonConst, n), sizeof(int));
+        SET_SPEC_CONST_ENTRY(entry[18], 18, offsetof(SpecializaitonConst, activation), sizeof(int));
+
+        VkSpecializationInfo spec_info;
+
+        spec_info.mapEntryCount = SPEC_CONST_NUM;
+        spec_info.pMapEntries   = entry;
+        spec_info.dataSize      = sizeof(spec_const);
+        spec_info.pData         = &spec_const;
 
         NN_GPU_DEBUG("run createShaderModule");
         opBase->createShaderModule(conv_spv, sizeof(conv_spv));
 
         NN_GPU_DEBUG("run createPipeline");
-        opBase->createPipeline(sizeof(ConvParameter));
+        opBase->createPipeline(sizeof(PushConst), &spec_info);
     }
 
-    opBase->group_x = alignSize(alignSize(N, config.block_width) / config.block_width, config.local_size_x) / config.local_size_x;
-    opBase->group_y = alignSize(alignSize(M, config.block_height) / config.block_height, config.local_size_y) / config.local_size_y;
-    opBase->group_z = alignSize(alignSize(param.batch, config.block_depth), config.local_size_z) / config.local_size_z;
+
+    opBase->group_x = alignSize(alignSize(N, config.block_width) / config.block_width, spec_const.local_sz_x) / spec_const.local_sz_x;
+    opBase->group_y = alignSize(alignSize(M, config.block_height) / config.block_height, spec_const.local_sz_y) / spec_const.local_sz_y;
+    opBase->group_z = alignSize(alignSize(spec_const.batch, config.block_depth), spec_const.local_sz_z) / spec_const.local_sz_z;
 
     NN_GPU_DEBUG("bind operands");
     opBase->bindOperand(in, 0, opBase->descriptor_set);
@@ -181,7 +237,7 @@ bool VkCsExecutor::convolve(const Operation& operation, ShaderConfig& config)
         for (int n = 0; n < partition_num; n++)
         {
             NN_GPU_DEBUG("do recordCommandBuffer");
-            opBase->recordCommandBuffer((void *)&param, sizeof(ConvParameter));
+            opBase->recordCommandBuffer((void*)&push_const, sizeof(PushConst));
 
             NN_GPU_DEBUG("do runCommandBuffer");
             opBase->runCommandBuffer();
